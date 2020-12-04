4cfceec8cfe0e1912176cdf3dcabc03b7f652656
Vianney Dubus <vianney.dubus@gmail.com>
2020-12-04 15:00:22 +0100

    Remove comments and old license information

--- src/main/java/fr/esupportail/esupstage/Application.java
+++ src/main/java/fr/esupportail/esupstage/Application.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.esupportail.esupstage;
 
 import org.springframework.boot.SpringApplication;
@@ -17,7 +12,7 @@ import fr.esupportail.esupstage.property.ApplicationProperties;
 @EnableConfigurationProperties(value = ApplicationProperties.class)
 public class Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(final String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
 
--- src/main/java/fr/esupportail/esupstage/domain/ldap/LdapUserRepository.java
+++ src/main/java/fr/esupportail/esupstage/domain/ldap/LdapUserRepository.java
@@ -12,31 +12,4 @@ public interface LdapUserRepository extends LdapRepository<LdapUser> {
 
 	Optional<LdapUser> findByAliasLogin(String aliasLogin);
 
-	// @Value("${ldap.attributs.id}")
-	// private String idAttr;
-	//
-	// @Value("${ldap.attributs.login}")
-	// private String loginAttr;
-	//
-	// @Value("${ldap.attributs}")
-	// private String attributs;
-	//
-	// @Autowired
-	// private LdapTemplate ldapTemplate;
-	//
-	// /**
-	// * @param id
-	// * @return l'utilisateur ldap correspondant à l'identifiant en paramètre
-	// */
-	// public LdapUser getUser(String id) {
-	// return ldapTemplate.findOne(query().attributes(attributs.split(",")).where(idAttr).is(id), LdapUser.class);
-	// }
-	//
-	// /**
-	// * @param login
-	// * @return l'utilisateur ldap correspondant au login en paramètre
-	// */
-	// public LdapUser getUserByAliasLogin(String login) {
-	// return ldapTemplate.findOne(query().attributes(attributs.split(",")).where(loginAttr).is(login), LdapUser.class);
-	// }
 }
