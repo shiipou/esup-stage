bc835d385c222e38d8c20d844a8741ca2fce86cf
Vianney Dubus <vianney.dubus@gmail.com>
2021-01-08 11:08:19 +0100

    Add JUnit for PstageVersManaRepository

--- src/main/java/fr/esupportail/esupstage/domain/jpa/entities/PstagedataVersMana.java
+++ src/main/java/fr/esupportail/esupstage/domain/jpa/entities/PstagedataVersMana.java
@@ -1,28 +1,36 @@
 package fr.esupportail.esupstage.domain.jpa.entities;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.io.Serializable;
-import javax.persistence.*;
 
 /**
  * The persistent class for the pstagedata_vers_mana database table.
  *
  */
 @Entity
-@Table(name = "pstagedata_vers_mana")
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "pstagedata_vers_mana")
 @NamedQuery(name = "PstagedataVersMana.findAll", query = "SELECT p FROM PstagedataVersMana p")
 public class PstagedataVersMana implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @Column(unique = true, nullable = false)
-    private String id;
-    @Column(length = 255)
-    private String vers;
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Column(unique = true, nullable = false)
+	private String id;
+
+	@Column(length = 255)
+	private String vers;
 
 }
---
+++ src/test/java/fr/esupportail/esupstage/domain/jpa/PstagedataVersManaRepositoryTest.java
@@ -0,0 +1,56 @@
+package fr.esupportail.esupstage.domain.jpa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import fr.esupportail.esupstage.AbstractTest;
+import fr.esupportail.esupstage.domain.jpa.entities.PstagedataVersMana;
+import fr.esupportail.esupstage.domain.jpa.repositories.PstagedataVersManaRepository;
+
+@Rollback
+@Transactional
+public class PstagedataVersManaRepositoryTest extends AbstractTest {
+
+	private final EntityManager entityManager;
+
+	private final PstagedataVersManaRepository repository;
+
+	@Autowired
+	PstagedataVersManaRepositoryTest(final EntityManager entityManager, final PstagedataVersManaRepository repository) {
+		super();
+		this.entityManager = entityManager;
+		this.repository = repository;
+	}
+
+	@BeforeEach
+	void prepare() {
+		final PstagedataVersMana entity = new PstagedataVersMana();
+		entity.setId("ID");
+		entity.setVers("1");
+
+		entityManager.persist(entity);
+		entityManager.flush();
+	}
+
+	@Test
+	@DisplayName("findById – Nominal test case")
+	void findById() {
+		final Optional<PstagedataVersMana> result = repository.findById("ID");
+		assertTrue(result.isPresent(), "We should have found our entity");
+
+		final PstagedataVersMana tmp = result.get();
+		assertEquals("1", tmp.getVers());
+	}
+
+}
