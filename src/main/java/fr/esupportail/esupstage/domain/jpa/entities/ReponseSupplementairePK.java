1d7b84e2c37a1074d95a84ab4551079b9fee8b8e
Vianney Dubus <vianney.dubus@gmail.com>
2021-01-08 16:21:16 +0100

    Add JUnit for ReponseSupplementaireRepository

--- src/main/java/fr/esupportail/esupstage/domain/jpa/entities/ReponseSupplementairePK.java
+++ src/main/java/fr/esupportail/esupstage/domain/jpa/entities/ReponseSupplementairePK.java
@@ -1,46 +1,30 @@
 package fr.esupportail.esupstage.domain.jpa.entities;
 
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.io.Serializable;
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * The primary key class for the ReponseSupplementaire database table.
  *
  */
+@Data
 @Embeddable
-@Getter
-@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ReponseSupplementairePK implements Serializable {
-    // default serial version id, required for serializable classes.
-    private static final long serialVersionUID = 1L;
-    @Column(insertable = false, updatable = false, unique = true, nullable = false)
-    private Integer idQuestionSupplementaire;
-    @Column(insertable = false, updatable = false, unique = true, nullable = false)
-    private Integer idConvention;
 
-    public boolean equals(Object other) {
-        if (this == other) {
-            return true;
-        }
-        if (!(other instanceof ReponseSupplementairePK)) {
-            return false;
-        }
-        ReponseSupplementairePK castOther = (ReponseSupplementairePK) other;
-        return (this.idQuestionSupplementaire == castOther.idQuestionSupplementaire)
-                && (this.idConvention == castOther.idConvention);
-    }
+	private static final long serialVersionUID = 1L;
 
-    public int hashCode() {
-        final Integer prime = 31;
-        Integer hash = 17;
-        hash = hash * prime + this.idQuestionSupplementaire;
-        hash = hash * prime + this.idConvention;
-        return hash;
-    }
+	@Column(insertable = false, updatable = false, unique = true, nullable = false)
+	private Integer idQuestionSupplementaire;
+
+	@Column(insertable = false, updatable = false, unique = true, nullable = false)
+	private Integer idConvention;
+
 }
---
+++ src/test/java/fr/esupportail/esupstage/domain/jpa/ReponseSupplementaireRepositoryTest.java
@@ -0,0 +1,183 @@
+package fr.esupportail.esupstage.domain.jpa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Calendar;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import fr.esupportail.esupstage.AbstractTest;
+import fr.esupportail.esupstage.domain.jpa.entities.CentreGestion;
+import fr.esupportail.esupstage.domain.jpa.entities.Confidentialite;
+import fr.esupportail.esupstage.domain.jpa.entities.Convention;
+import fr.esupportail.esupstage.domain.jpa.entities.Etudiant;
+import fr.esupportail.esupstage.domain.jpa.entities.FicheEvaluation;
+import fr.esupportail.esupstage.domain.jpa.entities.Indemnisation;
+import fr.esupportail.esupstage.domain.jpa.entities.LangueConvention;
+import fr.esupportail.esupstage.domain.jpa.entities.ModeValidationStage;
+import fr.esupportail.esupstage.domain.jpa.entities.NatureTravail;
+import fr.esupportail.esupstage.domain.jpa.entities.NiveauCentre;
+import fr.esupportail.esupstage.domain.jpa.entities.QuestionSupplementaire;
+import fr.esupportail.esupstage.domain.jpa.entities.ReponseSupplementaire;
+import fr.esupportail.esupstage.domain.jpa.entities.ReponseSupplementairePK;
+import fr.esupportail.esupstage.domain.jpa.entities.TempsTravail;
+import fr.esupportail.esupstage.domain.jpa.entities.Theme;
+import fr.esupportail.esupstage.domain.jpa.entities.TypeConvention;
+import fr.esupportail.esupstage.domain.jpa.repositories.ReponseSupplementaireRepository;
+
+@Rollback
+@Transactional
+public class ReponseSupplementaireRepositoryTest extends AbstractTest {
+
+	private final EntityManager entityManager;
+
+	private final ReponseSupplementaireRepository repository;
+
+	private ReponseSupplementairePK id;
+
+	@Autowired
+	ReponseSupplementaireRepositoryTest(final EntityManager entityManager, final ReponseSupplementaireRepository repository) {
+		super();
+		this.entityManager = entityManager;
+		this.repository = repository;
+	}
+
+	@BeforeEach
+	void prepare() {
+		final NiveauCentre niveauCentre = new NiveauCentre();
+		niveauCentre.setLibelleNiveauCentre("Label");
+		niveauCentre.setTemEnServNiveauCentre("A");
+		entityManager.persist(niveauCentre);
+
+		final Confidentialite confidentialite = new Confidentialite();
+		confidentialite.setCodeConfidentialite("A");
+		confidentialite.setLibelleConfidentialite("Label");
+		confidentialite.setTemEnServConfid("A");
+		entityManager.persist(confidentialite);
+
+		final CentreGestion centreGestion = new CentreGestion();
+		centreGestion.setAutorisationEtudiantCreationConvention(true);
+		centreGestion.setCodeUniversite("CodeUniv");
+		centreGestion.setDateCreation(Calendar.getInstance().getTime());
+		centreGestion.setIdModeValidationStage(1);
+		centreGestion.setLoginCreation("jdoe");
+		centreGestion.setConfidentialite(confidentialite);
+		centreGestion.setNiveauCentre(niveauCentre);
+		entityManager.persist(centreGestion);
+
+		final TypeConvention typeConvention = new TypeConvention();
+		typeConvention.setCodeCtrl("code");
+		typeConvention.setLibelleTypeConvention("Label");
+		typeConvention.setTemEnServTypeConvention("F");
+		entityManager.persist(typeConvention);
+
+		final Theme theme = new Theme();
+		theme.setLibelleTheme("Label");
+		entityManager.persist(theme);
+
+		final TempsTravail tempsTravail = new TempsTravail();
+		tempsTravail.setCodeCtrl("code");
+		tempsTravail.setLibelleTempsTravail("Label");
+		tempsTravail.setTemEnServTempsTravail("F");
+		entityManager.persist(tempsTravail);
+
+		final NatureTravail natureTravail = new NatureTravail();
+		natureTravail.setLibelleNatureTravail("Label");
+		natureTravail.setTemEnServNatTrav("F");
+		entityManager.persist(natureTravail);
+
+		final ModeValidationStage modeValidationStage = new ModeValidationStage();
+		modeValidationStage.setLibelleModeValidationStage("Label");
+		modeValidationStage.setTemEnServModeValid("F");
+		entityManager.persist(modeValidationStage);
+
+		final LangueConvention langueConvention = new LangueConvention();
+		langueConvention.setCodeLangueConvention("CD");
+		langueConvention.setLibelleLangueConvention("Label");
+		entityManager.persist(langueConvention);
+
+		final Indemnisation indemnisation = new Indemnisation();
+		indemnisation.setLibelleIndemnisation("indem");
+		indemnisation.setTemEnServIndem("F");
+		entityManager.persist(indemnisation);
+
+		final Etudiant etudiant = new Etudiant();
+		etudiant.setCodeUniversite("code");
+		etudiant.setDateCreation(Calendar.getInstance().getTime());
+		etudiant.setIdentEtudiant("ident");
+		etudiant.setLoginCreation("jdoe");
+		etudiant.setNom("Name");
+		etudiant.setNumEtudiant("125458");
+		etudiant.setPrenom("Firstname");
+		entityManager.persist(etudiant);
+
+		final FicheEvaluation ficheEvaluation = new FicheEvaluation();
+		ficheEvaluation.setCentreGestion(centreGestion);
+		entityManager.persist(ficheEvaluation);
+
+		final Convention convention = new Convention();
+		convention.setDateCreation(Calendar.getInstance().getTime());
+		convention.setDateDebutStage(Calendar.getInstance().getTime());
+		convention.setDateFinStage(Calendar.getInstance().getTime());
+		convention.setDureeStage(100);
+		convention.setIdAssurance(1);
+		convention.setIdModeVersGratification(1);
+		convention.setLoginCreation("jdoe");
+		convention.setNbJoursHebdo("1");
+		convention.setSujetStage("subject");
+		convention.setTemConfSujetTeme("s");
+		convention.setEtudiant(etudiant);
+		convention.setIndemnisation(indemnisation);
+		convention.setLangueConvention(langueConvention);
+		convention.setModeValidationStage(modeValidationStage);
+		convention.setNatureTravail(natureTravail);
+		convention.setTempsTravail(tempsTravail);
+		convention.setTheme(theme);
+		convention.setTypeConvention(typeConvention);
+		convention.setCentreGestion(centreGestion);
+		entityManager.persist(convention);
+
+		final QuestionSupplementaire questionSupplementaire = new QuestionSupplementaire();
+		questionSupplementaire.setIdPlacement(1);
+		questionSupplementaire.setQuestion("Question");
+		questionSupplementaire.setTypeQuestion("Type");
+		questionSupplementaire.setFicheEvaluation(ficheEvaluation);
+		entityManager.persist(questionSupplementaire);
+
+		entityManager.flush();
+
+		entityManager.refresh(convention);
+		entityManager.refresh(questionSupplementaire);
+
+		id = new ReponseSupplementairePK(questionSupplementaire.getId(), convention.getIdConvention());
+		final ReponseSupplementaire reponseSupplementaire = new ReponseSupplementaire();
+		reponseSupplementaire.setId(id);
+		reponseSupplementaire.setReponseTxt("Txt");
+		reponseSupplementaire.setReponseBool(true);
+		reponseSupplementaire.setReponseInt(666);
+		entityManager.persist(reponseSupplementaire);
+		entityManager.flush();
+	}
+
+	@Test
+	@DisplayName("findById – Nominal Test Case")
+	void findById() {
+		final Optional<ReponseSupplementaire> result = repository.findById(id);
+		assertTrue(result.isPresent(), "We should have found our entity");
+
+		final ReponseSupplementaire tmp = result.get();
+		assertTrue(tmp.isReponseBool());
+		assertEquals("Txt", tmp.getReponseTxt());
+		assertEquals(666, tmp.getReponseInt());
+	}
+
+}
