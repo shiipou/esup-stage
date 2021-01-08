8e7b00046bba9bb5464928b8db230502a40e8ae0
Vianney Dubus <vianney.dubus@gmail.com>
2021-01-08 17:00:31 +0100

    Add JUnit ServiceRepository

--- src/main/java/fr/esupportail/esupstage/domain/jpa/entities/Service.java
+++ src/main/java/fr/esupportail/esupstage/domain/jpa/entities/Service.java
@@ -1,104 +1,130 @@
 package fr.esupportail.esupstage.domain.jpa.entities;
 
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.io.Serializable;
-import javax.persistence.*;
-import java.util.Date;
-import java.util.List;
 
 /**
  * The persistent class for the Service database table.
  *
  */
 @Entity
-@Table(name = "Service")
 @Getter
 @Setter
-@NoArgsConstructor
+@Table(name = "Service")
 @NamedQuery(name = "Service.findAll", query = "SELECT s FROM Service s")
 public class Service extends Auditable<String> {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(unique = true, nullable = false)
-    private Integer idService;
-    @Column(length = 200)
-    private String batimentResidence;
-    @Column(length = 10)
-    private String codeCommune;
-    @Column(nullable = false, length = 10)
-    private String codePostal;
-    @Column(length = 200)
-    private String commune;
 
-    private Date infosAJour;
+	private static final long serialVersionUID = 1L;
 
-    @Column(length = 50)
-    private String loginInfosAJour;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(unique = true, nullable = false)
+	private Integer idService;
 
-    @Column(nullable = false, length = 70)
-    private String nom;
-    @Column(length = 20)
-    private String telephone;
-    @Column(nullable = false, length = 200)
-    private String voie;
-    // bi-directional many-to-one association to Avenant
-    @OneToMany(mappedBy = "service")
-    private List<Avenant> avenants;
-    // bi-directional many-to-one association to Contact
-    @OneToMany(mappedBy = "service")
-    private List<Contact> contacts;
-    // bi-directional many-to-one association to Convention
-    @OneToMany(mappedBy = "service")
-    private List<Convention> conventions;
-    // bi-directional many-to-one association to Pay
-    @ManyToOne
-    @JoinColumn(name = "idPays", nullable = false)
-    private Pays pay;
-    // bi-directional many-to-one association to Structure
-    @ManyToOne
-    @JoinColumn(name = "idStructure", nullable = false)
-    private Structure structure;
+	@Column(length = 200)
+	private String batimentResidence;
 
+	@Column(length = 10)
+	private String codeCommune;
 
-    public Avenant addAvenant(Avenant avenant) {
-        getAvenants().add(avenant);
-        avenant.setService(this);
-        return avenant;
-    }
+	@Column(nullable = false, length = 10)
+	private String codePostal;
 
-    public Avenant removeAvenant(Avenant avenant) {
-        getAvenants().remove(avenant);
-        avenant.setService(null);
-        return avenant;
-    }
+	@Column(length = 200)
+	private String commune;
 
+	private Date infosAJour;
 
-    public Contact addContact(Contact contact) {
-        getContacts().add(contact);
-        contact.setService(this);
-        return contact;
-    }
+	@Column(length = 50)
+	private String loginInfosAJour;
 
-    public Contact removeContact(Contact contact) {
-        getContacts().remove(contact);
-        contact.setService(null);
-        return contact;
-    }
+	@Column(nullable = false, length = 70)
+	private String nom;
 
-    public Convention addConvention(Convention convention) {
-        getConventions().add(convention);
-        convention.setService(this);
-        return convention;
-    }
+	@Column(length = 20)
+	private String telephone;
 
-    public Convention removeConvention(Convention convention) {
-        getConventions().remove(convention);
-        convention.setService(null);
-        return convention;
-    }
+	@Column(nullable = false, length = 200)
+	private String voie;
+
+	// bi-directional many-to-one association to Avenant
+	@OneToMany(mappedBy = "service")
+	private List<Avenant> avenants;
+
+	// bi-directional many-to-one association to Contact
+	@OneToMany(mappedBy = "service")
+	private List<Contact> contacts;
+
+	// bi-directional many-to-one association to Convention
+	@OneToMany(mappedBy = "service")
+	private List<Convention> conventions;
+
+	// bi-directional many-to-one association to Pay
+	@ManyToOne
+	@JoinColumn(name = "idPays", nullable = false)
+	private Pays pay;
+
+	// bi-directional many-to-one association to Structure
+	@ManyToOne
+	@JoinColumn(name = "idStructure", nullable = false)
+	private Structure structure;
+
+	public Service() {
+		super();
+		this.avenants = new LinkedList<>();
+		this.contacts = new LinkedList<>();
+		this.conventions = new LinkedList<>();
+	}
+
+	public Avenant addAvenant(Avenant avenant) {
+		getAvenants().add(avenant);
+		avenant.setService(this);
+		return avenant;
+	}
+
+	public Avenant removeAvenant(Avenant avenant) {
+		getAvenants().remove(avenant);
+		avenant.setService(null);
+		return avenant;
+	}
+
+	public Contact addContact(Contact contact) {
+		getContacts().add(contact);
+		contact.setService(this);
+		return contact;
+	}
+
+	public Contact removeContact(Contact contact) {
+		getContacts().remove(contact);
+		contact.setService(null);
+		return contact;
+	}
+
+	public Convention addConvention(Convention convention) {
+		getConventions().add(convention);
+		convention.setService(this);
+		return convention;
+	}
+
+	public Convention removeConvention(Convention convention) {
+		getConventions().remove(convention);
+		convention.setService(null);
+		return convention;
+	}
+
 }
---
+++ src/test/java/fr/esupportail/esupstage/domain/jpa/ServiceRepositoryTest.java
@@ -0,0 +1,103 @@
+package fr.esupportail.esupstage.domain.jpa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Calendar;
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.Rollback;
+
+import fr.esupportail.esupstage.AbstractTest;
+import fr.esupportail.esupstage.domain.jpa.entities.Effectif;
+import fr.esupportail.esupstage.domain.jpa.entities.Pays;
+import fr.esupportail.esupstage.domain.jpa.entities.Service;
+import fr.esupportail.esupstage.domain.jpa.entities.Structure;
+import fr.esupportail.esupstage.domain.jpa.entities.TypeStructure;
+import fr.esupportail.esupstage.domain.jpa.repositories.ServiceRepository;
+
+@Rollback
+@Transactional
+@WithMockUser(username = "jdoe", password = "jdoe")
+public class ServiceRepositoryTest extends AbstractTest {
+
+	private final EntityManager entityManager;
+
+	private final ServiceRepository repository;
+
+	private Integer id;
+
+	@Autowired
+	ServiceRepositoryTest(final EntityManager entityManager, final ServiceRepository repository) {
+		super();
+		this.entityManager = entityManager;
+		this.repository = repository;
+	}
+
+	@BeforeEach
+	void prepare() {
+		final Pays pays = new Pays();
+		pays.setActual(1);
+		pays.setCog(1);
+		pays.setLib("Label");
+		pays.setSiretObligatoire(true);
+		pays.setTemEnServPays("A");
+		entityManager.persist(pays);
+
+		final Effectif effectif = new Effectif();
+		effectif.setLibelleEffectif("Label");
+		effectif.setTemEnServEffectif("A");
+		entityManager.persist(effectif);
+
+		final TypeStructure typeStructure = new TypeStructure();
+		typeStructure.setTemEnServTypeStructure("A");
+		typeStructure.setLibelleTypeStructure("Label");
+		typeStructure.setSiretObligatoire(true);
+		entityManager.persist(typeStructure);
+
+		final Structure structure = new Structure();
+		structure.setDateCreation(Calendar.getInstance().getTime());
+		structure.setEstValidee(1);
+		structure.setLoginCreation("jdoe");
+		structure.setRaisonSociale("ESN");
+		structure.setVoie("Street");
+		structure.setPay(pays);
+		structure.setEffectif(effectif);
+		structure.setTypeStructure(typeStructure);
+		entityManager.persist(structure);
+
+		final Service service = new Service();
+		service.setCodePostal("59000");
+		service.setNom("Service");
+		service.setVoie("Street");
+		service.setPay(pays);
+		service.setStructure(structure);
+		entityManager.persist(service);
+		entityManager.flush();
+
+		entityManager.refresh(service);
+		id = service.getIdService();
+	}
+
+	@Test
+	@DisplayName("findById – Nominal Test Case")
+	void findById() {
+		final Optional<Service> result = repository.findById(id);
+		assertTrue(result.isPresent(), "We should have found our entity");
+
+		final Service tmp = result.get();
+		assertEquals("jdoe", tmp.getCreatedBy());
+		assertEquals("59000", tmp.getCodePostal());
+		assertEquals("Service", tmp.getNom());
+		assertEquals("Street", tmp.getVoie());
+	}
+
+}
