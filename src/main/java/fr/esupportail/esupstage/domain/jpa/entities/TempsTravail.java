b48994fe2f26c7013399b4132071394bb8b6535c
Vianney Dubus <vianney.dubus@gmail.com>
2021-01-14 14:51:43 +0100

    Add JUnit for TempsTravailRepository

--- src/main/java/fr/esupportail/esupstage/domain/jpa/entities/TempsTravail.java
+++ src/main/java/fr/esupportail/esupstage/domain/jpa/entities/TempsTravail.java
@@ -1,13 +1,20 @@
 package fr.esupportail.esupstage.domain.jpa.entities;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.io.Serializable;
-import javax.persistence.*;
-import java.util.List;
 
 /**
  * The persistent class for the TempsTravail database table.
@@ -20,47 +27,53 @@ import java.util.List;
 @Table(name = "TempsTravail")
 @NamedQuery(name = "TempsTravail.findAll", query = "SELECT t FROM TempsTravail t")
 public class TempsTravail implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(unique = true, nullable = false)
-    private Integer idTempsTravail;
-    @Column(nullable = false, length = 20)
-    private String codeCtrl;
-    @Column(nullable = false, length = 200)
-    private String libelleTempsTravail;
-    private boolean modifiable;
-    @Column(nullable = false, length = 1)
-    private String temEnServTempsTravail;
-    // bi-directional many-to-one association to Convention
-    @OneToMany(mappedBy = "tempsTravail")
-    private List<Convention> conventions;
-    // bi-directional many-to-one association to Offre
-    @OneToMany(mappedBy = "tempsTravail")
-    private List<Offre> offres;
 
+	private static final long serialVersionUID = 1L;
 
-    public Convention addConvention(Convention convention) {
-        getConventions().add(convention);
-        convention.setTempsTravail(this);
-        return convention;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(unique = true, nullable = false)
+	private Integer idTempsTravail;
 
-    public Convention removeConvention(Convention convention) {
-        getConventions().remove(convention);
-        convention.setTempsTravail(null);
-        return convention;
-    }
+	@Column(nullable = false, length = 20)
+	private String codeCtrl;
 
-    public Offre addOffre(Offre offre) {
-        getOffres().add(offre);
-        offre.setTempsTravail(this);
-        return offre;
-    }
+	@Column(nullable = false, length = 200)
+	private String libelleTempsTravail;
 
-    public Offre removeOffre(Offre offre) {
-        getOffres().remove(offre);
-        offre.setTempsTravail(null);
-        return offre;
-    }
+	private boolean modifiable;
+
+	@Column(nullable = false, length = 1)
+	private String temEnServTempsTravail;
+
+	@OneToMany(mappedBy = "tempsTravail")
+	private List<Convention> conventions;
+
+	@OneToMany(mappedBy = "tempsTravail")
+	private List<Offre> offres;
+
+	public Convention addConvention(Convention convention) {
+		getConventions().add(convention);
+		convention.setTempsTravail(this);
+		return convention;
+	}
+
+	public Convention removeConvention(Convention convention) {
+		getConventions().remove(convention);
+		convention.setTempsTravail(null);
+		return convention;
+	}
+
+	public Offre addOffre(Offre offre) {
+		getOffres().add(offre);
+		offre.setTempsTravail(this);
+		return offre;
+	}
+
+	public Offre removeOffre(Offre offre) {
+		getOffres().remove(offre);
+		offre.setTempsTravail(null);
+		return offre;
+	}
+
 }
---
+++ src/test/java/fr/esupportail/esupstage/domain/jpa/TempsTravailRepositoryTest.java
@@ -0,0 +1,63 @@
+package fr.esupportail.esupstage.domain.jpa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import fr.esupportail.esupstage.AbstractTest;
+import fr.esupportail.esupstage.domain.jpa.entities.TempsTravail;
+import fr.esupportail.esupstage.domain.jpa.repositories.TempsTravailRepository;
+
+@Rollback
+@Transactional
+public class TempsTravailRepositoryTest extends AbstractTest {
+
+	private final EntityManager entityManager;
+
+	private final TempsTravailRepository repository;
+
+	private Integer id;
+
+	@Autowired
+	TempsTravailRepositoryTest(final EntityManager entityManager, final TempsTravailRepository repository) {
+		super();
+		this.entityManager = entityManager;
+		this.repository = repository;
+	}
+
+	@BeforeEach
+	void prepare() {
+		final TempsTravail tempsTravail = new TempsTravail();
+		tempsTravail.setCodeCtrl("Code");
+		tempsTravail.setLibelleTempsTravail("Label");
+		tempsTravail.setTemEnServTempsTravail("A");
+		entityManager.persist(tempsTravail);
+
+		entityManager.flush();
+		entityManager.refresh(tempsTravail);
+		id = tempsTravail.getIdTempsTravail();
+	}
+
+	@Test
+	@DisplayName("findById – Nominal Test Case")
+	void findById() {
+		final Optional<TempsTravail> result = repository.findById(id);
+		assertTrue(result.isPresent(), "We should have found our entity");
+
+		final TempsTravail tmp = result.get();
+		assertEquals("Code", tmp.getCodeCtrl());
+		assertEquals("Label", tmp.getLibelleTempsTravail());
+		assertEquals("A", tmp.getTemEnServTempsTravail());
+	}
+
+}
