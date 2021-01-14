0b6aca0c475ab1170b1010db15019afcbb381e4a
Vianney Dubus <vianney.dubus@gmail.com>
2021-01-14 16:42:02 +0100

    Add JUnit for ThemeRepository

--- src/main/java/fr/esupportail/esupstage/domain/jpa/entities/Theme.java
+++ src/main/java/fr/esupportail/esupstage/domain/jpa/entities/Theme.java
@@ -1,47 +1,59 @@
 package fr.esupportail.esupstage.domain.jpa.entities;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.io.Serializable;
-import javax.persistence.*;
-import java.util.List;
 
 /**
  * The persistent class for the Theme database table.
  *
  */
 @Entity
-@Table(name = "Theme")
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "Theme")
 @NamedQuery(name = "Theme.findAll", query = "SELECT t FROM Theme t")
 public class Theme implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(unique = true, nullable = false)
-    private Integer idTheme;
-    @Column(nullable = false, length = 50)
-    private String libelleTheme;
-    @Column(length = 1)
-    private String temEnServTheme;
-    // bi-directional many-to-one association to Convention
-    @OneToMany(mappedBy = "theme")
-    private List<Convention> conventions;
 
-    public Convention addConvention(Convention convention) {
-        getConventions().add(convention);
-        convention.setTheme(this);
-        return convention;
-    }
+	private static final long serialVersionUID = 1L;
 
-    public Convention removeConvention(Convention convention) {
-        getConventions().remove(convention);
-        convention.setTheme(null);
-        return convention;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(unique = true, nullable = false)
+	private Integer idTheme;
+
+	@Column(nullable = false, length = 50)
+	private String libelleTheme;
+
+	@Column(length = 1)
+	private String temEnServTheme;
+
+	@OneToMany(mappedBy = "theme")
+	private List<Convention> conventions;
+
+	public Convention addConvention(Convention convention) {
+		getConventions().add(convention);
+		convention.setTheme(this);
+		return convention;
+	}
+
+	public Convention removeConvention(Convention convention) {
+		getConventions().remove(convention);
+		convention.setTheme(null);
+		return convention;
+	}
+
 }
---
+++ src/test/java/fr/esupportail/esupstage/domain/jpa/ThemeRepositoryTest.java
@@ -0,0 +1,61 @@
+package fr.esupportail.esupstage.domain.jpa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import fr.esupportail.esupstage.AbstractTest;
+import fr.esupportail.esupstage.domain.jpa.entities.Theme;
+import fr.esupportail.esupstage.domain.jpa.repositories.ThemeRepository;
+
+@Rollback
+@Transactional
+public class ThemeRepositoryTest extends AbstractTest {
+
+	private final EntityManager entityManager;
+
+	private final ThemeRepository repository;
+
+	private Integer id;
+
+	@Autowired
+	ThemeRepositoryTest(final EntityManager entityManager, final ThemeRepository repository) {
+		super();
+		this.entityManager = entityManager;
+		this.repository = repository;
+	}
+
+	@BeforeEach
+	void prepare() {
+		final Theme theme = new Theme();
+		theme.setLibelleTheme("Label");
+		theme.setTemEnServTheme("A");
+		entityManager.persist(theme);
+
+		entityManager.flush();
+		entityManager.refresh(theme);
+		id = theme.getIdTheme();
+	}
+
+	@Test
+	@DisplayName("findById – Nominal Test Case")
+	void findById() {
+		final Optional<Theme> result = repository.findById(id);
+		assertTrue(result.isPresent(), "We should have found our entity");
+
+		final Theme tmp = result.get();
+		assertEquals("Label", tmp.getLibelleTheme());
+		assertEquals("A", tmp.getTemEnServTheme());
+	}
+
+}
