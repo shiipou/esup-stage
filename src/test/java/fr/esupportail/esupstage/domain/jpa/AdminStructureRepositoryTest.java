95ee717d193f562e943884d0365573cc9a965a1d
Joshua Baubry <joshua.baubry@gmail.com>
2021-01-15 14:39:02 +0100

    fix test AdminStructureRepositoryTest

--- src/test/java/fr/esupportail/esupstage/domain/jpa/AdminStructureRepositoryTest.java
+++ src/test/java/fr/esupportail/esupstage/domain/jpa/AdminStructureRepositoryTest.java
@@ -8,24 +8,31 @@ import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import fr.esupportail.esupstage.domain.jpa.entities.*;
+import fr.esupportail.esupstage.domain.jpa.repositories.AccordPartenariatRepository;
+import fr.esupportail.esupstage.domain.jpa.repositories.AdminStructureRepository;
+import org.eclipse.jdt.internal.compiler.env.IModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.data.domain.Example;
 import org.springframework.test.annotation.Rollback;
 
 import fr.esupportail.esupstage.AbstractTest;
 import fr.esupportail.esupstage.domain.jpa.entities.AdminStructure;
 import fr.esupportail.esupstage.domain.jpa.repositories.AdminStructureRepository;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @Rollback
 @Transactional
 class AdminStructureRepositoryTest extends AbstractTest {
 
 	private final EntityManager entityManager;
-
 	private final AdminStructureRepository adminStructureRepository;
+	private int adminStructureId;
 
 	@Autowired
 	AdminStructureRepositoryTest(final EntityManager entityManager, final AdminStructureRepository adminStructureRepository) {
@@ -43,21 +50,19 @@ class AdminStructureRepositoryTest extends AbstractTest {
 
 		entityManager.persist(adminStructure);
 		entityManager.flush();
-
+		this.adminStructureId = adminStructure.getIdAdminStructure();
 	}
 
-	@Test
-	@DisplayName("findById – Nominal test case")
-	void findById() {
-		final AdminStructure adminStructure = new AdminStructure();
-		adminStructure.setDateCreation(new Date(0));
-		adminStructure.setLoginCreation("login");
+    @Test
+    @DisplayName("findById – Nominal test case")
+    void findById() {
 
-		final Optional<AdminStructure> result = adminStructureRepository.findOne(Example.of(adminStructure));
-		assertTrue(result.isPresent(), "We should have found our teacher");
+        final Optional<AdminStructure> result = adminStructureRepository.findById(adminStructureId);
+        assertTrue(result.isPresent(), "We should have found our teacher");
 
-		final AdminStructure adminStructure1 = result.get();
-
-	}
+        final AdminStructure adminStructure = result.get();
+        assertEquals(new Date(0),adminStructure.getDateCreation());
+        assertEquals("login",adminStructure.getLoginCreation());
+    }
 
 }
