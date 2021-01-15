e09d4435ece4ef4ad392f9906df7ea6d1e096d4a
Vianney Dubus <vianney.dubus@gmail.com>
2021-01-15 10:24:38 +0100

    Add JUnit for TypeConventionRepository

--- src/main/java/fr/esupportail/esupstage/domain/jpa/entities/TypeConvention.java
+++ src/main/java/fr/esupportail/esupstage/domain/jpa/entities/TypeConvention.java
@@ -1,50 +1,64 @@
 package fr.esupportail.esupstage.domain.jpa.entities;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.io.Serializable;
-import javax.persistence.*;
-import java.util.List;
 
 /**
  * The persistent class for the TypeConvention database table.
  *
  */
 @Entity
-@Table(name = "TypeConvention")
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "TypeConvention")
 @NamedQuery(name = "TypeConvention.findAll", query = "SELECT t FROM TypeConvention t")
 public class TypeConvention implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(unique = true, nullable = false)
-    private Integer idTypeConvention;
-    @Column(nullable = false, length = 20)
-    private String codeCtrl;
-    @Column(nullable = false, length = 50)
-    private String libelleTypeConvention;
-    private boolean modifiable;
-    @Column(nullable = false, length = 1)
-    private String temEnServTypeConvention;
-    // bi-directional many-to-one association to Convention
-    @OneToMany(mappedBy = "typeConvention")
-    private List<Convention> conventions;
 
-    public Convention addConvention(Convention convention) {
-        getConventions().add(convention);
-        convention.setTypeConvention(this);
-        return convention;
-    }
+	private static final long serialVersionUID = 1L;
 
-    public Convention removeConvention(Convention convention) {
-        getConventions().remove(convention);
-        convention.setTypeConvention(null);
-        return convention;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(unique = true, nullable = false)
+	private Integer idTypeConvention;
+
+	@Column(nullable = false, length = 20)
+	private String codeCtrl;
+
+	@Column(nullable = false, length = 50)
+	private String libelleTypeConvention;
+
+	private boolean modifiable;
+
+	@Column(nullable = false, length = 1)
+	private String temEnServTypeConvention;
+
+	@OneToMany(mappedBy = "typeConvention")
+	private List<Convention> conventions;
+
+	public Convention addConvention(Convention convention) {
+		getConventions().add(convention);
+		convention.setTypeConvention(this);
+		return convention;
+	}
+
+	public Convention removeConvention(Convention convention) {
+		getConventions().remove(convention);
+		convention.setTypeConvention(null);
+		return convention;
+	}
+
 }
---
+++ src/test/java/fr/esupportail/esupstage/domain/jpa/TypeConventionRepositoryTest.java
@@ -0,0 +1,63 @@
+package fr.esupportail.esupstage.domain.jpa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import fr.esupportail.esupstage.AbstractTest;
+import fr.esupportail.esupstage.domain.jpa.entities.TypeConvention;
+import fr.esupportail.esupstage.domain.jpa.repositories.TypeConventionRepository;
+
+@Rollback
+@Transactional
+public class TypeConventionRepositoryTest extends AbstractTest {
+
+	private final EntityManager entityManager;
+
+	private final TypeConventionRepository repository;
+
+	private Integer id;
+
+	@Autowired
+	TypeConventionRepositoryTest(final EntityManager entityManager, final TypeConventionRepository repository) {
+		super();
+		this.entityManager = entityManager;
+		this.repository = repository;
+	}
+
+	@BeforeEach
+	void prepare() {
+		final TypeConvention typeConvention = new TypeConvention();
+		typeConvention.setCodeCtrl("Code");
+		typeConvention.setLibelleTypeConvention("Label");
+		typeConvention.setTemEnServTypeConvention("A");
+		entityManager.persist(typeConvention);
+
+		entityManager.flush();
+		entityManager.refresh(typeConvention);
+		id = typeConvention.getIdTypeConvention();
+	}
+
+	@Test
+	@DisplayName("findById – Nominal Test Case")
+	void findById() {
+		final Optional<TypeConvention> result = repository.findById(id);
+		assertTrue(result.isPresent(), "We should have found our entity");
+
+		final TypeConvention tmp = result.get();
+		assertEquals("Code", tmp.getCodeCtrl());
+		assertEquals("A", tmp.getTemEnServTypeConvention());
+		assertEquals("Label", tmp.getLibelleTypeConvention());
+	}
+
+}
