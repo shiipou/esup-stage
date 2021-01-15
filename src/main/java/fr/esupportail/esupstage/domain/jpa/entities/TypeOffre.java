1fe4ee594a9920334fed119e4fb6cad5ebe09baa
Vianney Dubus <vianney.dubus@gmail.com>
2021-01-15 11:41:40 +0100

    Add JUnit for TypeOffreRepository

--- src/main/java/fr/esupportail/esupstage/domain/jpa/entities/TypeOffre.java
+++ src/main/java/fr/esupportail/esupstage/domain/jpa/entities/TypeOffre.java
@@ -1,67 +1,79 @@
 package fr.esupportail.esupstage.domain.jpa.entities;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.io.Serializable;
-import javax.persistence.*;
-import java.util.List;
 
 /**
  * The persistent class for the TypeOffre database table.
  *
  */
 @Entity
-@Table(name = "TypeOffre")
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "TypeOffre")
 @NamedQuery(name = "TypeOffre.findAll", query = "SELECT t FROM TypeOffre t")
 public class TypeOffre implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(unique = true, nullable = false)
-    private Integer idTypeOffre;
-    @Column(nullable = false, length = 20)
-    private String codeCtrl;
-    @Column(nullable = false, length = 50)
-    private String libelleType;
-    private boolean modifiable;
-    @Column(nullable = false, length = 1)
-    private String temEnServTypeOffre;
-    // bi-directional many-to-one association to ContratOffre
-    @OneToMany(mappedBy = "typeOffre")
-    private List<ContratOffre> contratOffres;
-    // bi-directional many-to-one association to Offre
-    @OneToMany(mappedBy = "typeOffre")
-    private List<Offre> offres;
 
+	private static final long serialVersionUID = 1L;
 
-    public ContratOffre addContratOffre(ContratOffre contratOffre) {
-        getContratOffres().add(contratOffre);
-        contratOffre.setTypeOffre(this);
-        return contratOffre;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(unique = true, nullable = false)
+	private Integer idTypeOffre;
 
-    public ContratOffre removeContratOffre(ContratOffre contratOffre) {
-        getContratOffres().remove(contratOffre);
-        contratOffre.setTypeOffre(null);
-        return contratOffre;
-    }
+	@Column(nullable = false, length = 20)
+	private String codeCtrl;
 
+	@Column(nullable = false, length = 50)
+	private String libelleType;
 
-    public Offre addOffre(Offre offre) {
-        getOffres().add(offre);
-        offre.setTypeOffre(this);
-        return offre;
-    }
+	private boolean modifiable;
 
-    public Offre removeOffre(Offre offre) {
-        getOffres().remove(offre);
-        offre.setTypeOffre(null);
-        return offre;
-    }
+	@Column(nullable = false, length = 1)
+	private String temEnServTypeOffre;
+
+	@OneToMany(mappedBy = "typeOffre")
+	private List<ContratOffre> contratOffres;
+
+	@OneToMany(mappedBy = "typeOffre")
+	private List<Offre> offres;
+
+	public ContratOffre addContratOffre(ContratOffre contratOffre) {
+		getContratOffres().add(contratOffre);
+		contratOffre.setTypeOffre(this);
+		return contratOffre;
+	}
+
+	public ContratOffre removeContratOffre(ContratOffre contratOffre) {
+		getContratOffres().remove(contratOffre);
+		contratOffre.setTypeOffre(null);
+		return contratOffre;
+	}
+
+	public Offre addOffre(Offre offre) {
+		getOffres().add(offre);
+		offre.setTypeOffre(this);
+		return offre;
+	}
+
+	public Offre removeOffre(Offre offre) {
+		getOffres().remove(offre);
+		offre.setTypeOffre(null);
+		return offre;
+	}
+
 }
---
+++ src/test/java/fr/esupportail/esupstage/domain/jpa/TypeOffreRepositoryTest.java
@@ -0,0 +1,63 @@
+package fr.esupportail.esupstage.domain.jpa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import fr.esupportail.esupstage.AbstractTest;
+import fr.esupportail.esupstage.domain.jpa.entities.TypeOffre;
+import fr.esupportail.esupstage.domain.jpa.repositories.TypeOffreRepository;
+
+@Rollback
+@Transactional
+public class TypeOffreRepositoryTest extends AbstractTest {
+
+	private final EntityManager entityManager;
+
+	private final TypeOffreRepository repository;
+
+	private Integer id;
+
+	@Autowired
+	TypeOffreRepositoryTest(final EntityManager entityManager, final TypeOffreRepository repository) {
+		super();
+		this.entityManager = entityManager;
+		this.repository = repository;
+	}
+
+	@BeforeEach
+	void prepare() {
+		final TypeOffre typeOffre = new TypeOffre();
+		typeOffre.setCodeCtrl("Code");
+		typeOffre.setLibelleType("Label");
+		typeOffre.setTemEnServTypeOffre("A");
+		entityManager.persist(typeOffre);
+
+		entityManager.flush();
+		entityManager.refresh(typeOffre);
+		id = typeOffre.getIdTypeOffre();
+	}
+
+	@Test
+	@DisplayName("findById – Nominal Test Case")
+	void findById() {
+		final Optional<TypeOffre> result = repository.findById(id);
+		assertTrue(result.isPresent(), "We should have found our entity");
+
+		final TypeOffre tmp = result.get();
+		assertEquals("Code", tmp.getCodeCtrl());
+		assertEquals("Label", tmp.getLibelleType());
+		assertEquals("A", tmp.getTemEnServTypeOffre());
+	}
+
+}
