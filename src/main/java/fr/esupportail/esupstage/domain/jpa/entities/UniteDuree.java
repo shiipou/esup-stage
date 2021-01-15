9871d3fd96b49176c22b895e19a0993498a070af
Vianney Dubus <vianney.dubus@gmail.com>
2021-01-15 14:14:28 +0100

    Add JUnit for UniteDureeRepository

--- src/main/java/fr/esupportail/esupstage/domain/jpa/entities/UniteDuree.java
+++ src/main/java/fr/esupportail/esupstage/domain/jpa/entities/UniteDuree.java
@@ -1,65 +1,74 @@
 package fr.esupportail.esupstage.domain.jpa.entities;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.io.Serializable;
-import javax.persistence.*;
-import java.util.List;
 
 /**
  * The persistent class for the UniteDuree database table.
  *
  */
 @Entity
-@Table(name = "UniteDuree")
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "UniteDuree")
 @NamedQuery(name = "UniteDuree.findAll", query = "SELECT u FROM UniteDuree u")
 public class UniteDuree implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(unique = true, nullable = false)
-    private Integer idUniteDuree;
-    @Column(nullable = false, length = 100)
-    private String libelleUniteDuree;
-    @Column(nullable = false, length = 1)
-    private String temEnServUniteDuree;
-    // bi-directional many-to-one association to Convention
-    @OneToMany(mappedBy = "uniteDuree")
-    private List<Convention> conventions;
-    // bi-directional many-to-one association to Offre
-    @OneToMany(mappedBy = "uniteDuree")
-    private List<Offre> offres;
 
+	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(unique = true, nullable = false)
+	private Integer idUniteDuree;
 
-    public Convention addConvention(Convention convention) {
-        getConventions().add(convention);
-        convention.setUniteDuree(this);
-        return convention;
-    }
+	@Column(nullable = false, length = 100)
+	private String libelleUniteDuree;
 
-    public Convention removeConvention(Convention convention) {
-        getConventions().remove(convention);
-        convention.setUniteDuree(null);
-        return convention;
-    }
+	@Column(nullable = false, length = 1)
+	private String temEnServUniteDuree;
 
+	@OneToMany(mappedBy = "uniteDuree")
+	private List<Convention> conventions;
 
-    public Offre addOffre(Offre offre) {
-        getOffres().add(offre);
-        offre.setUniteDuree(this);
-        return offre;
-    }
+	@OneToMany(mappedBy = "uniteDuree")
+	private List<Offre> offres;
 
-    public Offre removeOffre(Offre offre) {
-        getOffres().remove(offre);
-        offre.setUniteDuree(null);
-        return offre;
-    }
+	public Convention addConvention(Convention convention) {
+		getConventions().add(convention);
+		convention.setUniteDuree(this);
+		return convention;
+	}
+
+	public Convention removeConvention(Convention convention) {
+		getConventions().remove(convention);
+		convention.setUniteDuree(null);
+		return convention;
+	}
+
+	public Offre addOffre(Offre offre) {
+		getOffres().add(offre);
+		offre.setUniteDuree(this);
+		return offre;
+	}
+
+	public Offre removeOffre(Offre offre) {
+		getOffres().remove(offre);
+		offre.setUniteDuree(null);
+		return offre;
+	}
+
 }
---
+++ src/test/java/fr/esupportail/esupstage/domain/jpa/UniteDureeRepositoryTest.java
@@ -0,0 +1,61 @@
+package fr.esupportail.esupstage.domain.jpa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import fr.esupportail.esupstage.AbstractTest;
+import fr.esupportail.esupstage.domain.jpa.entities.UniteDuree;
+import fr.esupportail.esupstage.domain.jpa.repositories.UniteDureeRepository;
+
+@Rollback
+@Transactional
+public class UniteDureeRepositoryTest extends AbstractTest {
+
+	private final EntityManager entityManager;
+
+	private final UniteDureeRepository repository;
+
+	private Integer id;
+
+	@Autowired
+	UniteDureeRepositoryTest(final EntityManager entityManager, final UniteDureeRepository repository) {
+		super();
+		this.entityManager = entityManager;
+		this.repository = repository;
+	}
+
+	@BeforeEach
+	void prepare() {
+		final UniteDuree uniteDuree = new UniteDuree();
+		uniteDuree.setLibelleUniteDuree("Label");
+		uniteDuree.setTemEnServUniteDuree("A");
+		entityManager.persist(uniteDuree);
+
+		entityManager.flush();
+		entityManager.refresh(uniteDuree);
+		id = uniteDuree.getIdUniteDuree();
+	}
+
+	@Test
+	@DisplayName("findById – Nominal Test Case")
+	void findById() {
+		final Optional<UniteDuree> result = repository.findById(id);
+		assertTrue(result.isPresent(), "We should have found our entity");
+
+		final UniteDuree tmp = result.get();
+		assertEquals("Label", tmp.getLibelleUniteDuree());
+		assertEquals("A", tmp.getTemEnServUniteDuree());
+	}
+
+}
