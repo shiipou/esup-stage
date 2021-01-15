8698fcf118c44663c62f327bd43f8590f80e4a6a
Vianney Dubus <vianney.dubus@gmail.com>
2021-01-15 11:54:47 +0100

    Add JUnit for TypeStructureRepository

--- src/main/java/fr/esupportail/esupstage/domain/jpa/entities/TypeStructure.java
+++ src/main/java/fr/esupportail/esupstage/domain/jpa/entities/TypeStructure.java
@@ -1,65 +1,79 @@
 package fr.esupportail.esupstage.domain.jpa.entities;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.io.Serializable;
-import javax.persistence.*;
-import java.util.List;
 
 /**
  * The persistent class for the TypeStructure database table.
  *
  */
 @Entity
-@Table(name = "TypeStructure")
 @Getter
 @Setter
 @NoArgsConstructor
+@Table(name = "TypeStructure")
 @NamedQuery(name = "TypeStructure.findAll", query = "SELECT t FROM TypeStructure t")
 public class TypeStructure implements Serializable {
-    private static final long serialVersionUID = 1L;
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(unique = true, nullable = false)
-    private Integer idTypeStructure;
-    @Column(nullable = false, length = 100)
-    private String libelleTypeStructure;
-    private boolean modifiable;
-    @Column(nullable = false)
-    private boolean siretObligatoire;
-    @Column(nullable = false, length = 1)
-    private String temEnServTypeStructure;
-    // bi-directional many-to-one association to StatutJuridique
-    @OneToMany(mappedBy = "typeStructure")
-    private List<StatutJuridique> statutJuridiques;
-    // bi-directional many-to-one association to Structure
-    @OneToMany(mappedBy = "typeStructure")
-    private List<Structure> structures;
 
-    public StatutJuridique addStatutJuridique(StatutJuridique statutJuridique) {
-        getStatutJuridiques().add(statutJuridique);
-        statutJuridique.setTypeStructure(this);
-        return statutJuridique;
-    }
+	private static final long serialVersionUID = 1L;
 
-    public StatutJuridique removeStatutJuridique(StatutJuridique statutJuridique) {
-        getStatutJuridiques().remove(statutJuridique);
-        statutJuridique.setTypeStructure(null);
-        return statutJuridique;
-    }
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(unique = true, nullable = false)
+	private Integer idTypeStructure;
 
-    public Structure addStructure(Structure structure) {
-        getStructures().add(structure);
-        structure.setTypeStructure(this);
-        return structure;
-    }
+	@Column(nullable = false, length = 100)
+	private String libelleTypeStructure;
 
-    public Structure removeStructure(Structure structure) {
-        getStructures().remove(structure);
-        structure.setTypeStructure(null);
-        return structure;
-    }
+	private boolean modifiable;
+
+	@Column(nullable = false)
+	private boolean siretObligatoire;
+
+	@Column(nullable = false, length = 1)
+	private String temEnServTypeStructure;
+
+	@OneToMany(mappedBy = "typeStructure")
+	private List<StatutJuridique> statutJuridiques;
+
+	@OneToMany(mappedBy = "typeStructure")
+	private List<Structure> structures;
+
+	public StatutJuridique addStatutJuridique(StatutJuridique statutJuridique) {
+		getStatutJuridiques().add(statutJuridique);
+		statutJuridique.setTypeStructure(this);
+		return statutJuridique;
+	}
+
+	public StatutJuridique removeStatutJuridique(StatutJuridique statutJuridique) {
+		getStatutJuridiques().remove(statutJuridique);
+		statutJuridique.setTypeStructure(null);
+		return statutJuridique;
+	}
+
+	public Structure addStructure(Structure structure) {
+		getStructures().add(structure);
+		structure.setTypeStructure(this);
+		return structure;
+	}
+
+	public Structure removeStructure(Structure structure) {
+		getStructures().remove(structure);
+		structure.setTypeStructure(null);
+		return structure;
+	}
+
 }
---
+++ src/test/java/fr/esupportail/esupstage/domain/jpa/TypeStructureRepositoryTest.java
@@ -0,0 +1,63 @@
+package fr.esupportail.esupstage.domain.jpa;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+
+import fr.esupportail.esupstage.AbstractTest;
+import fr.esupportail.esupstage.domain.jpa.entities.TypeStructure;
+import fr.esupportail.esupstage.domain.jpa.repositories.TypeStructureRepository;
+
+@Rollback
+@Transactional
+public class TypeStructureRepositoryTest extends AbstractTest {
+
+	private final EntityManager entityManager;
+
+	private final TypeStructureRepository repository;
+
+	private Integer id;
+
+	@Autowired
+	TypeStructureRepositoryTest(final EntityManager entityManager, final TypeStructureRepository repository) {
+		super();
+		this.entityManager = entityManager;
+		this.repository = repository;
+	}
+
+	@BeforeEach
+	void prepare() {
+		final TypeStructure typeStructure = new TypeStructure();
+		typeStructure.setLibelleTypeStructure("Label");
+		typeStructure.setSiretObligatoire(true);
+		typeStructure.setTemEnServTypeStructure("A");
+		entityManager.persist(typeStructure);
+
+		entityManager.flush();
+		entityManager.refresh(typeStructure);
+		id = typeStructure.getIdTypeStructure();
+	}
+
+	@Test
+	@DisplayName("findById – Nominal Test Case")
+	void findById() {
+		final Optional<TypeStructure> result = repository.findById(id);
+		assertTrue(result.isPresent(), "We should have found our entity");
+
+		final TypeStructure tmp = result.get();
+		assertTrue(tmp.isSiretObligatoire());
+		assertEquals("Label", tmp.getLibelleTypeStructure());
+		assertEquals("A", tmp.getTemEnServTypeStructure());
+	}
+
+}
